e831b7172d7e13be72af8d24fee89d19ee1b45fe
Eric Kang <erickangatms@outlook.com>
2017-10-31 07:34:11 -0700

    .

---
+++ utils/cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -f ./src/tables/*
+rm -f ./src/views/*
+rm -f ./src/types/*
+rm -f ./src/functions/*
+rm -f ./src/procedures/*
+rm -f ./src/security/*
+rm -f ./src/security/*
+rm -f ./src/misc/*
